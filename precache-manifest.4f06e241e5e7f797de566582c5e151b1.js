a8192735ad8279890d2e9d23ca0155a943435d4b
Leonid Rusakov <bengunn87@mail.ru>
2020-01-11 23:14:08 +0500

    Updates

---
+++ precache-manifest.4f06e241e5e7f797de566582c5e151b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a348ea249a8bb1fa50722d90ffb391",
+    "url": "/countries_info/index.html"
+  },
+  {
+    "revision": "960d9f74fc34b25e07a0",
+    "url": "/countries_info/static/css/main.9dba8910.chunk.css"
+  },
+  {
+    "revision": "33a605a618f57ba6b8c5",
+    "url": "/countries_info/static/js/2.2f908042.chunk.js"
+  },
+  {
+    "revision": "960d9f74fc34b25e07a0",
+    "url": "/countries_info/static/js/main.92111480.chunk.js"
+  },
+  {
+    "revision": "4c1eb722c242e291db71",
+    "url": "/countries_info/static/js/runtime-main.1fd1544d.js"
+  }
+]);
