183abaf994a991c69f338bd3da8c929901f4d57f
Leonid Rusakov <bengunn87@mail.ru>
2020-01-12 11:47:01 +0500

    Доработал компонент LanguageSelect

--- src/components/LanguageSelect/LanguageSelect.tsx
+++ src/components/LanguageSelect/LanguageSelect.tsx
@@ -1,21 +1,23 @@
-import {languageStore} from '../../stores';
 import React from "react";
 import {IDictionary} from "../../type/common.type";
+import {inject, observer} from "mobx-react";
+import {LanguageStore} from "../../stores/LanguageStore";
 
-export const LanguageSelect: React.FunctionComponent = () => {
-    const {setCurrentLanguage, currentLanguage, dictionary} = languageStore;
+export const LanguageSelect = inject('languageStore') (observer((props: {languageStore?: LanguageStore}) => {
+    const {setCurrentLanguage, currentLanguage, dictionary} = props.languageStore!;
     const optionList = Object.entries(dictionary).map(([key, value]) => {
-        return <option value={key} key={key}>{value.name}</option>
+        return <option
+            value={key}
+            key={key}
+        >
+            {value.name}
+        </option>
     });
 
     return <select
         value={currentLanguage}
-        onChange={
-            (event) => {
-                setCurrentLanguage(event.target.value as keyof IDictionary);
-            }
-        }
+        onChange={(event) => {setCurrentLanguage(event.target.value as keyof IDictionary);}}
     >
         {optionList}
     </select>
-};
+}));
